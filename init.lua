caa106021d38fa7203c07b8698469a54a10d2a50
Fatih Gungor <dfgungor@gmail.com>
2025-06-10 03:49:53 +0200

    feat: neovide config

--- init.lua
+++ init.lua
@@ -1,3 +1,13 @@
 require("config.options")
 require("config.keybinds")
 require("config.lazy")
+
+if vim.g.neovide then
+	-- Put anything you want to happen only in Neovide here
+	vim.g.neovide_opacity = 1.0
+	vim.g.neovide_normal_opacity = 0.95
+	vim.g.neovide_cursor_trail_size = 0.0
+	vim.g.neovide_cursor_animation_length = 0.05
+	vim.g.neovide_fullscreen = true
+	vim.o.guifont = "JetBrainsMono NFM"
+end
